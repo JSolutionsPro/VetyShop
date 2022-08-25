147adc0e01378378909c339afa9229c13b2006e5
ChrisWolff <107091056+SirCWolff@users.noreply.github.com>
2022-08-25 12:11:09 -0500

    Creación de la clase MovimientosDinero y tipos de datos

---
+++ src/main/java/com/apolosolutions/Apolo/Controlador/Controlador.java
@@ -0,0 +1,12 @@
+package com.apolosolutions.Apolo.Controlador;
+
+import com.apolosolutions.Apolo.Modelos.MovimientosDinero;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class Controlador {
+    @GetMapping(GetMapping("/pruebas")
+            public String pruebas() {})
+    MovimientosDinero
+}
