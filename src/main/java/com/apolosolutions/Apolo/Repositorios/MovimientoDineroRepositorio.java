3dfc9f43f6d6b0b9bb64526ea083a2c4db88f092
Jose Daniel Soto C <soto.jose@correounivalle.edu.co>
2022-09-24 04:35:45 -0500

    Mejoras en Movimientos, controladores para ver mov de la empresa y usuario en sesion

--- src/main/java/com/apolosolutions/Apolo/Repositorios/MovimientoDineroRepositorio.java
+++ src/main/java/com/apolosolutions/Apolo/Repositorios/MovimientoDineroRepositorio.java
@@ -1,6 +1,8 @@
 package com.apolosolutions.Apolo.Repositorios;
 
 import com.apolosolutions.Apolo.Modelos.MovimientoDinero;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,11 +13,11 @@ import java.util.ArrayList;
 public interface MovimientoDineroRepositorio extends JpaRepository<MovimientoDinero, Integer> {
 
     @Query(value ="SELECT * FROM transacciones WHERE usuario_id= ?1", nativeQuery = true)
-    public abstract ArrayList<MovimientoDinero> findByUsuario(Integer id);
+    public abstract Page<MovimientoDinero> findByUsuario(Integer id, Pageable pageable);
 
     //Metodo para filtrar movimientos de un usuario por empresa
     @Query(value="SELECT * FROM transacciones WHERE usuario_id IN (SELECT id FROM usuario WHERE empresa_id= ?1)", nativeQuery = true)
-    public abstract ArrayList<MovimientoDinero> findMovimientosOfUsuariosByEmpresa(Integer id);
+    public abstract Page<MovimientoDinero> findMovimientosOfUsuariosByEmpresa(Integer id, Pageable pageable);
 
     @Query(value ="SELECT * FROM transacciones WHERE empresa_id= ?1", nativeQuery = true)
     public abstract ArrayList<MovimientoDinero> findByEmpresa(Integer id);
--- src/main/java/com/apolosolutions/Apolo/Servicios/MovimientoDineroServicios.java
+++ src/main/java/com/apolosolutions/Apolo/Servicios/MovimientoDineroServicios.java
@@ -4,6 +4,8 @@ import com.apolosolutions.Apolo.Modelos.MovimientoDinero;
 import com.apolosolutions.Apolo.Repositorios.EmpresaRepositorio;
 import com.apolosolutions.Apolo.Repositorios.MovimientoDineroRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,8 +17,8 @@ public class MovimientoDineroServicios {
         @Autowired
         EmpresaRepositorio empresaRepositorio;
 
-        public List<MovimientoDinero> ListarMovimientos(){
-                return movimientoDineroRepositorio.findAll();
+        public Page<MovimientoDinero> ListarMovimientos(Pageable pageable){
+                return movimientoDineroRepositorio.findAll(pageable);
         }
         public MovimientoDinero consultarMovimiento(Integer id){
                 return movimientoDineroRepositorio.findById(id).get();
@@ -38,8 +40,8 @@ public class MovimientoDineroServicios {
                 return true;
         }
 
-        public ArrayList<MovimientoDinero> consultarPorUsuario(Integer id) {
-                return movimientoDineroRepositorio.findByUsuario(id);
+        public Page<MovimientoDinero> consultarPorUsuario(Integer id, Pageable pageable) {
+                return movimientoDineroRepositorio.findByUsuario(id, pageable);
         }
         public ArrayList<MovimientoDinero> consultarPorEmpresa(Integer id){
                 return movimientoDineroRepositorio.findByEmpresa(id);
@@ -60,8 +62,8 @@ public class MovimientoDineroServicios {
                 return false;
         }
 
-        public List<MovimientoDinero> consultarMovimientosDeUsuariosPorEmpresa(Integer id){
-                return movimientoDineroRepositorio.findMovimientosOfUsuariosByEmpresa(id);
+        public Page<MovimientoDinero> consultarMovimientosDeUsuariosPorEmpresa(Integer id, Pageable pageable){
+                return movimientoDineroRepositorio.findMovimientosOfUsuariosByEmpresa(id, pageable);
         }
 
         public Long obtenerSumaMovimientos(){
